423094a474ad32b4cd7387db12e89f1f8d5dc749
Artem <laz-mail@mail.ru>
2018-10-19 18:35:31 +0300

    рабочий вариант

--- Main.go
+++ Main.go
@@ -18,12 +18,9 @@ import (
 )
 
 type Data struct {
-	db_Name string
-	context string
-	event   string
-	value   int64
-	count   int
-	OutStr  string
+	value  int64
+	count  int
+	OutStr string
 }
 
 type IChain interface {
@@ -35,6 +32,40 @@ type Chain struct {
 	NextElement    *Chain
 	regexp         *regexp.Regexp
 	AgregateFileld []string
+}
+
+var ChainPool = sync.Pool{
+	New: func() interface{} {
+		return BuildChain()
+	},
+}
+
+//type mapData map[string]*Data
+var FullData map[string]*Data
+var Mutex = &sync.Mutex{}
+var SortByCount, SortByValue, IO bool
+var Top, Go int
+var RootDir string
+var DataBuffer [10]*Data
+
+func main() {
+	FullData = make(map[string]*Data)
+	flag.BoolVar(&SortByCount, "SortByCount", false, "Сортировка по количеству вызовов (bool)")
+	flag.BoolVar(&SortByValue, "SortByValue", false, "Сортировка по значению (bool)")
+	flag.BoolVar(&IO, "io", false, "Флаг того, что данные будут поступать из StdIn (bool)")
+	flag.IntVar(&Top, "Top", 100, "Ограничение на вывод по количеству записей")
+	flag.IntVar(&Go, "Go", 10, "Количество воркеров которые будут обрабатывать файл")
+	flag.StringVar(&RootDir, "RootDir", "", "Корневая директория")
+	flag.Parse()
+
+	if RootDir != "" {
+		FindFiles(RootDir)
+	} else if IO {
+		readStdIn()
+	} else {
+		panic("Не определены входящие данные")
+	}
+
 }
 
 func (c *Chain) Execute(SourceStr string) (string, string, int64) {
@@ -70,7 +101,8 @@ func (c *Chain) Execute(SourceStr string) (string, string, int64) {
 			}
 		}
 
-		if k != "" {
+		// Value потом будет агригороваться
+		if k != "" && k != "Value" {
 			OutStr = strings.Replace(OutStr, "%"+k+"%", strings.Trim(v, " \n"), -1)
 		}
 	}
@@ -79,29 +111,11 @@ func (c *Chain) Execute(SourceStr string) (string, string, int64) {
 	return getHash(KeyStr), OutStr, v
 }
 
-//type mapData map[string]*Data
-var FullData map[string]*Data
-var Mutex = &sync.Mutex{}
-var SortByCount, SortByValue bool
-var Top int
-var Go int
+//////////////////////////// Горутины ////////////////////////////////////////
 
-func main() {
-	FullData = make(map[string]*Data)
-	flag.BoolVar(&SortByCount, "SortByCount", false, "Сортировка по количеству вызовов (bool)")
-	flag.BoolVar(&SortByValue, "SortByValue", false, "Сортировка по значению (bool)")
-	flag.IntVar(&Top, "Top", 100, "Ограничение на вывод по количеству записей (int)")
-	flag.IntVar(&Go, "Go", 10, "Количество воркеров которые будут обрабатывать файл (int)")
-	flag.Parse()
-
-	//FindFiles(`D:\1C_Log\КБР\1C_log`)
-	FindFiles(`D:\1C_Log\Lazarenko`)
-}
-
-func runWorkers(count int, inChan <-chan *string, outChan chan<- map[string]*Data, group *sync.WaitGroup) {
-	for i := 0; i < count; i++ {
-		group.Add(1)
-		go startWorker(inChan, outChan, group)
+func goReader(outChan <-chan map[string]*Data) {
+	for input := range outChan {
+		MergeData(input)
 	}
 }
 
@@ -114,7 +128,74 @@ func startWorker(inChan <-chan *string, outChan chan<- map[string]*Data, group *
 	}
 }
 
+func ParsFile(FilePath string, group *sync.WaitGroup) {
+	defer group.Done()
+
+	var file *os.File
+	defer file.Close()
+	file, er := os.Open(FilePath)
+
+	if er != nil {
+		fmt.Printf("Ошибка открытия файла %v\n\t%v", FilePath, er.Error())
+		return
+	}
+
+	Scan := bufio.NewScanner(file)
+	ParsStream(Scan)
+}
+
+func putInQueue(key string, value *Data) {
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+func runWorkers(count int, inChan <-chan *string, outChan chan<- map[string]*Data, group *sync.WaitGroup) {
+	for i := 0; i < count; i++ {
+		group.Add(1)
+		go startWorker(inChan, outChan, group)
+	}
+}
+
+func BuildChain() *Chain {
+	Element0 := Chain{
+		regexp:         regexp.MustCompile(`(?si)[\d]+:[\d]+\.[\d]+[-](?P<Value>[\d]+)[,](?P<event>[^,]+)(?:.*?)p:processName=(?P<DB>[^,]+)(?:.+?)Module=(?P<Module>[^,]+)(?:.+?)Method=(?P<Method>[^,]+)`),
+		AgregateFileld: []string{"event", "DB", "Module", "Method"},
+		OutPattern:     "(%DB%) %event%, количество - %count%, duration - %Value%\n%Module%.%Method%",
+	}
+
+	Element1 := Chain{
+		regexp:         regexp.MustCompile(`(?si)[\d]+:[\d]+\.[\d]+[-](?P<Value>[\d]+)[,](?P<event>[^,]+)(?:.*?)p:processName=(?P<DB>[^,]+)(?:.+?)(Context=(?P<Context>[^,]+))`),
+		NextElement:    &Element0,
+		AgregateFileld: []string{"event", "DB", "Context"},
+		OutPattern:     "(%DB%) %event%, количество - %count%, duration - %Value%\n%Context%",
+	}
+
+	/* 	Element2 := Chain{
+		regexp:         regexp.MustCompile(`(?si)[,]CALL(?:.*?)p:processName=(?P<DB>[^,]+)(?:.+?)Context=(?P<Context>[^,]+)(?:.+?)MemoryPeak=(?P<Value>[\d]+)`),
+		NextElement:    &Element1,
+		AgregateFileld: []string{"DB", "Context"},
+		OutPattern:     "(%DB%) CALL, количество - %count%, MemoryPeak - %Value%\n%Context%",
+	} */
+
+	Element3 := Chain{
+		regexp:         regexp.MustCompile(`(?si)[,]EXCP,(?:.*?)process=(?P<Process>[^,]+)(?:.*?)Descr=(?P<Context>[^,]+)`),
+		NextElement:    &Element1,
+		AgregateFileld: []string{"Process", "Context"},
+		OutPattern:     "(%Process%) EXCP, количество - %count%\n%Context%",
+	}
+	return &Element3
+}
+
+func readStdIn() {
+	in := bufio.NewScanner(os.Stdin)
+	ParsStream(in)
+
+	PrettyPrint()
+}
+
 func FindFiles(rootDir string) {
+
 	start := time.Now()
 	group := &sync.WaitGroup{}
 	callBack := func(path string, info os.FileInfo, err error) error {
@@ -125,7 +206,6 @@ func FindFiles(rootDir string) {
 		if strings.HasSuffix(info.Name(), "log") {
 			group.Add(1)
 			go ParsFile(path, group)
-			//fmt.Printf("тек. каталог %q\n\tфайл %v\n", path, info.Name())
 		}
 		return nil
 	}
@@ -139,15 +219,9 @@ func FindFiles(rootDir string) {
 	fmt.Printf("Код выполнялся: %v\n", elapsed)
 }
 
-func goReader(outChan <-chan map[string]*Data) {
-	for input := range outChan {
-		MergeData(input)
-	}
-}
-
 func MergeData(Data map[string]*Data) {
 	for key, value := range Data {
-		//FData := PoolData.Get().(map[string]*Data)
+		//go putInQueue(key, value)
 
 		Mutex.Lock()
 		if _, exist := FullData[key]; exist {
@@ -157,13 +231,10 @@ func MergeData(Data map[string]*Data) {
 			FullData[key] = value
 		}
 		Mutex.Unlock()
-		//PoolData.Put(FData)
 	}
 }
 
-func ParsFile(FilePath string, group *sync.WaitGroup) {
-	defer group.Done()
-
+func ParsStream(Scan *bufio.Scanner) {
 	inChan := make(chan *string, Go)
 	outChan := make(chan map[string]*Data, Go)
 	localgroup := &sync.WaitGroup{} // Группа для ожидания выполнения пула воркеров.
@@ -171,19 +242,11 @@ func ParsFile(FilePath string, group *sync.WaitGroup) {
 	pattern := `(?mi)\d\d:\d\d\.\d+[-]\d+`
 	re := regexp.MustCompile(pattern)
 
-	var file *os.File
-	defer file.Close()
-	file, er := os.Open(FilePath)
-
-	if er != nil {
-		fmt.Printf("Ошибка открытия файла %v\n\t%v", FilePath, er.Error())
-		return
+	runWorkers(Go, inChan, outChan, localgroup)
+	for i := 0; i < Go; i++ {
+		go goReader(outChan) // Отдельная горутина которая будет читать из канала.
 	}
 
-	runWorkers(Go, inChan, outChan, localgroup)
-	go goReader(outChan) // Отдельная горутина которая будет читать из канала.
-
-	Scan := bufio.NewScanner(file)
 	buff := make([]string, 1)
 	writeChan := func() {
 		part := strings.Join(buff, "\n")
@@ -210,6 +273,7 @@ func ParsFile(FilePath string, group *sync.WaitGroup) {
 	close(inChan) // Закрываем канал на для чтения
 
 	localgroup.Wait()
+
 	close(outChan)
 }
 
@@ -234,10 +298,9 @@ func PrettyPrint() {
 	for id := range array[:Top] {
 		OutStr := array[id].OutStr
 		OutStr = strings.Replace(OutStr, "%count%", fmt.Sprintf("%d", array[id].count), -1)
-		OutStr = strings.Replace(OutStr, "%value%", fmt.Sprintf("%d", array[id].value), -1)
+		OutStr = strings.Replace(OutStr, "%Value%", fmt.Sprintf("%d", array[id].value), -1)
 
-		fmt.Println(OutStr)
-		//fmt.Printf("(%v) %v count - %v, duration - %v\n\t%v\n", array[id].db_Name, array[id].event, array[id].count, array[id].value, array[id].context)
+		fmt.Println(OutStr + "\n")
 	}
 }
 
@@ -247,20 +310,8 @@ func ParsPart(Blob *string) map[string]*Data {
 		return nil
 	}
 
-	Element0 := Chain{
-		regexp:         regexp.MustCompile(`(?si)[\d]+:[\d]+\.[\d]+[-](?P<Value>[\d]+)[,](?P<event>[^,]+)(?:.*?)p:processName=(?P<DB>[^,]+)(?:.+?)Module=(?P<Module>[^,]+)(?:.+?)Method=(?P<Method>[^,]+)`),
-		AgregateFileld: []string{"event", "DB", "Module", "Method"},
-		OutPattern:     "(%DB%) %event%, количество - %count%, duration - %value%\n%Module%.%Method%",
-	}
-
-	Element1 := Chain{
-		regexp:         regexp.MustCompile(`(?si)[\d]+:[\d]+\.[\d]+[-](?P<Value>[\d]+)[,](?P<event>[^,]+)(?:.*?)p:processName=(?P<DB>[^,]+)(?:.+?)(Context=(?P<Context>[^,]+))`),
-		NextElement:    &Element0,
-		AgregateFileld: []string{"event", "DB", "Context"},
-		OutPattern:     "(%DB%) %event%, количество - %count%, duration - %value%\n%Context%",
-	}
-
-	key, data, value := Element1.Execute(Str)
+	RepChain := ChainPool.Get().(*Chain)
+	key, data, value := RepChain.Execute(Str)
 	result := Data{OutStr: data, value: value, count: 1}
 	return map[string]*Data{getHash(key): &result}
 }
