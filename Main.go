7b4ce28c460469ee3e1266153c921954ba79643c
Artem <laz-mail@mail.ru>
2018-10-23 23:37:56 +0300

    разбил по каталогам

--- Main.go
+++ Main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"bufio"
-	"crypto/md5"
-	"crypto/rand"
 	"encoding/gob"
 	"flag"
 	"fmt"
@@ -15,22 +13,22 @@ import (
 	"regexp"
 	"runtime"
 	"sort"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
 
 	"runtime/pprof"
+
+	. "./Chain"
+	. "./Tools"
 )
+
+//var Tools, error = build.Import("Tools/Chain", "", build.IgnoreVendor)
 
 type Data struct {
 	value  int64
 	count  int
 	OutStr string
-}
-
-type IChain interface {
-	Execute(SourceStr string) (string, string, int64)
 }
 
 type ImapData interface {
@@ -40,20 +38,6 @@ type ImapData interface {
 type IProfTime interface {
 	Start() *ProfTime
 	Stop()
-}
-
-type Chain struct {
-	OutPattern     string
-	NextElement    *Chain
-	preCondition   func(in string) bool
-	regexp         *regexp.Regexp
-	AgregateFileld []string
-}
-
-var ChainPool = sync.Pool{
-	New: func() interface{} {
-		return BuildChain()
-	},
 }
 
 type ProfTime struct {
@@ -208,7 +192,7 @@ func ParsPart(Blob *string, RepChain IChain) mapData {
 	   	data := ""
 	   	var value int64 = 0 */
 	result := Data{OutStr: data, value: value, count: 1}
-	return mapData{getHash(key): &result}
+	return mapData{GetHash(key): &result}
 }
 
 //////////////////////////// Профилирование //////////////////////////////////
@@ -310,7 +294,7 @@ func ParsFile(FilePath string, mergeChan chan<- mapData, Chain *Chain, group *sy
 
 func (d *mapData) SerializationAndSave(TempDir string) {
 
-	file, err2 := os.Create(path.Join(TempDir, uuid()))
+	file, err2 := os.Create(path.Join(TempDir, Uuid()))
 	defer file.Close()
 	if err2 != nil {
 		fmt.Println("Ошибка создания файла:\n", err2.Error())
@@ -400,28 +384,6 @@ func GetFiles(DirPath string) ([]string, int64) {
 	return result, size
 }
 
-func uuid() string {
-	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return ""
-	} else {
-		return fmt.Sprintf("%X", b)
-	}
-}
-
-func getHash(inStr string) string {
-	UnifyString := ToUnify(inStr)
-	Sum := md5.Sum([]byte(UnifyString))
-	return fmt.Sprintf("%x", Sum)
-}
-
-func ToUnify(inStr string) string {
-	re := regexp.MustCompile(`[\n\s'\.;:\(\)\"\'\d]`)
-	return re.ReplaceAllString(inStr, "")
-}
-
 func FindFiles(rootDir string) {
 	if _, err := os.Stat(rootDir); os.IsNotExist(err) {
 		panic(fmt.Sprintf("Файл %q не существует", rootDir))
@@ -461,90 +423,6 @@ func FindFiles(rootDir string) {
 	mergeGroup.Wait()
 	close(resultChan)
 	resultGroup.Wait()
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-//////////////////////////// Цепочки /////////////////////////////////////////
-
-func BuildChain() *Chain {
-	/* Element0 := Chain{
-		regexp:         regexp.MustCompile(`(?si)[\d]+:[\d]+\.[\d]+[-](?P<Value>[\d]+)[,]CALL(?:.*?)p:processName=(?P<DB>[^,]+)(?:.+?)Module=(?P<Module>[^,]+)(?:.+?)Method=(?P<Method>[^,]+)`),
-		AgregateFileld: []string{"event", "DB", "Module", "Method"},
-		OutPattern:     "(%DB%) CALL, количество - %count%, duration - %Value%\n%Module%.%Method%",
-	}
-
-	Element1 := Chain{
-		regexp:         regexp.MustCompile(`(?si)[\d]+:[\d]+\.[\d]+[-](?P<Value>[\d]+)[,](?P<event>[^,]+)(?:.*?)p:processName=(?P<DB>[^,]+)(?:.+?)(Context=(?P<Context>[^,]+))`),
-		NextElement:    &Element0,
-		AgregateFileld: []string{"event", "DB", "Context"},
-		OutPattern:     "(%DB%) %event%, количество - %count%, duration - %Value%\n%Context%",
-	} */
-
-	Element1 := Chain{
-		//preCondition:   func(In string) bool { return strings.Contains(In, ",CALL") },
-		regexp:         regexp.MustCompile(`(?si)[,]CALL(?:.*?)p:processName=(?P<DB>[^,]+)(?:.+?)Module=(?P<Module>[^,]+)(?:.+?)Method=(?P<Method>[^,]+)(?:.+?)MemoryPeak=(?P<Value>[\d]+)`),
-		AgregateFileld: []string{"event", "DB", "Module", "Method"},
-		OutPattern:     "(%DB%) CALL, количество - %count%, MemoryPeak - %Value%\n%Module%.%Method%",
-	}
-
-	Element2 := Chain{
-		regexp:         regexp.MustCompile(`(?si)[,]CALL(?:.*?)p:processName=(?P<DB>[^,]+)(?:.+?)Context=(?P<Context>[^,]+)(?:.+?)MemoryPeak=(?P<Value>[\d]+)`),
-		NextElement:    &Element1,
-		AgregateFileld: []string{"DB", "Context"},
-		OutPattern:     "(%DB%) CALL, количество - %count%, MemoryPeak - %Value%\n%Context%",
-	}
-
-	Element3 := Chain{
-		regexp:         regexp.MustCompile(`(?si)[,]EXCP,(?:.*?)process=(?P<Process>[^,]+)(?:.*?)Descr=(?P<Context>[^,]+)`),
-		NextElement:    &Element2,
-		AgregateFileld: []string{"Process", "Context"},
-		OutPattern:     "(%Process%) EXCP, количество - %count%\n%Context%",
-	}
-	return &Element3
-}
-
-func (c *Chain) Execute(SourceStr string) (string, string, int64) {
-	exRegExp := func() map[string]string {
-		matches := c.regexp.FindStringSubmatch(SourceStr)
-		if len(matches) == 0 {
-			return nil
-		}
-
-		// GroupsName - для работы с именоваными группами захвата.
-		GroupsName := make(map[string]string)
-		for id, name := range c.regexp.SubexpNames() {
-			GroupsName[name] = matches[id]
-		}
-
-		return GroupsName
-	}
-
-	result := exRegExp()
-	if result == nil {
-		if c.NextElement != nil {
-			return c.NextElement.Execute(SourceStr)
-		}
-		return "", "", 0
-	}
-
-	var KeyStr string
-	var OutStr string = c.OutPattern
-	for k, v := range result {
-		for _, j := range c.AgregateFileld {
-			if j == k {
-				KeyStr += v
-			}
-		}
-
-		// Value потом будет агригороваться
-		if k != "" && k != "Value" {
-			OutStr = strings.Replace(OutStr, "%"+k+"%", strings.Trim(v, " \n"), -1)
-		}
-	}
-
-	v, _ := strconv.ParseInt(result["Value"], 10, 64)
-	return getHash(KeyStr), OutStr, v
 }
 
 //////////////////////////////////////////////////////////////////////////////
---
+++ Tools/CommonTools.go
@@ -0,0 +1,30 @@
+package CommonTools
+
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"fmt"
+	"regexp"
+)
+
+func GetHash(inStr string) string {
+	UnifyString := toUnify(inStr)
+	Sum := md5.Sum([]byte(UnifyString))
+	return fmt.Sprintf("%x", Sum)
+}
+
+func toUnify(inStr string) string {
+	re := regexp.MustCompile(`[\n\s'\.;:\(\)\"\'\d]`)
+	return re.ReplaceAllString(inStr, "")
+}
+
+func Uuid() string {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return ""
+	} else {
+		return fmt.Sprintf("%X", b)
+	}
+}
